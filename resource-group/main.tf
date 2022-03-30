bb5bca565c0a32b216a163e86ca52a169c96b0e6
asharsidd <asharsidd@gmail.com>
2022-03-30 15:01:03 +0100

    applied plan

--- resource-group/main.tf
+++ resource-group/main.tf
@@ -1,1 +1,33 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  resource_group = var.resource_group_name
+  location = var.location
+}
+
+# Deploy two Resource Groups
+resource "azurerm_resource_group" "rg" {
+  count = 2
+  name  = "${local.resource_group}-${count.index+1}"
+  location = local.location
+  
+tags = {
+   Environment = "Terrform IAC"
+   Team = "CIS"
+}
+}
 
---
+++ resource-group/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "RGIPT001"
+  description = " Resource Group name"
+  type = string
+}
+
+variable "location" {
+  default = "eastus"
+  description = "Location for Resource deployment"
+  type = string
+}
